b81fc3fca43ac6dfa5030cadcfba48b1edb61e1d
Seif Ahmed <seifahmeed@hotmail.com>
2020-10-17 22:50:50 +0200

    First

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/*global $*/
+$(document).ready(function () {
+    'use strict';
+    // Trigger Slick
+    $('.ex-boxs').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+    
+    $(window).on("load", function () {
+        if ($(this).width() < 768) {
+            $('header').css("backgroundImage", "url(../img/bg2.png)");
+        } else {
+            $('header').css("backgroundImage", "url(../img/bg.png)");
+        }
+    });
+    $(window).on("resize", function () {
+        if ($(this).width() < 768) {
+            $('header').css("backgroundImage", "url(../img/bg2.png)");
+        } else {
+            $('header').css("backgroundImage", "url(../img/bg.png)");
+        }
+    });
+    
+});
